17a3f8b51b1f802ab23ca3e44b65104e19507ce2
VagTsiats <vagtsiats12@gmail.com>
2024-05-23 17:55:13 +0300

    room pcd dataset

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 import numpy as np
 import open3d as o3d
+import scipy as sp
+import h5py
 
 if __name__ == "__main__":
 
     o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel.Error)
 
-    pcd = o3d.io.read_triangle_model("dataset/living_room_obj_mtl/living-room.obj")
-    # print(pcd.meshes[10].mesh)
+    # model = o3d.io.read_triangle_model("dataset/area_3/3d/rgb.obj")
+    # mesh = o3d.geometry.TriangleMesh()
 
-    mesh = o3d.geometry.TriangleMesh()
+    # for m in model.meshes:
+    #     mesh += m.mesh
 
-    for m in pcd.meshes:
-        # print(m)
-        mesh += m.mesh
+    # mat = h5py.File("dataset/area_3/3d/pointcloud.mat")
+    # roomptr = mat["Area_3"]["Disjoint_Space"]["object"][14][0]
+    # pcd = o3d.geometry.PointCloud()
 
+    # for obj in mat[roomptr]["points"]:
+    #     pts = np.asarray(mat[obj[0]])
+
+    #     pcd.points.extend(pts.T)
+
+    # R = mesh.get_rotation_matrix_from_xyz((np.deg2rad(270), 0, 0))
+    # pcd.rotate(R, center=(0, 0, 0))
+
+    mesh = o3d.io.read_triangle_mesh("dataset/area_3/3d/rgb.obj")
+    pcd = o3d.io.read_point_cloud("dataset/area_3/Area_3/office_9/office_9.txt", format="xyz")
+
+    R = pcd.get_rotation_matrix_from_xyz((np.deg2rad(-90), 0, 0))
+    pcd.rotate(R, center=(0, 0, 0))
+
+    print(pcd)
     print(mesh)
 
-    # downpcd = pcd.voxel_down_sample(voxel_size=0.05)
+    # mesh.compute_triangle_normals()
+    mesh.compute_vertex_normals()
 
-    mesh.compute_triangle_normals()
+    # print(mesh.cluster_connected_triangles()[0])
 
-    # print(np.asarray(mesh.triangle_normals))
+    # pcd = mesh.sample_points_poisson_disk(500)
 
-    o3d.visualization.draw_geometries([mesh])
+    o3d.visualization.draw_geometries([mesh, pcd])
